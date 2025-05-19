7b4abd276adbcd36d4dfa361fb82649e4ae28ea0
Fernanda Quintero <fernanda.quindu0830@gmail.com>
2025-05-19 15:50:03 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,18 @@
+function mostrarAstros() {
+      let astroSeleccionado = document.getElementById("astros").value
+      console.log(astroSeleccionado)
+      document.getElementById("astroImg").src = astroSeleccionado
+    }
+function acercaDe() {
+      alert("Pagina Programada por Fernanda Quintero 2025")
+    }
+
+  function terminar() {
+      self.close()
+    }
+       
+      
+    
+
+
+        
